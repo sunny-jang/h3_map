02d186b049f8bcd3cf946f63838b0c2953c86767
sunny-jang <esunbest@naver.com>
2023-01-24 03:25:14 +0900

    맵 옵션들 분리

---
+++ src/constants.ts
@@ -0,0 +1,20 @@
+export const mapOptions = {
+    center: new window.naver.maps.LatLng(37.544186, 127.044127),
+    zoom: 11,
+};
+
+export const newPolygonOptions = {
+    fillColor: "#0000ff",
+    fillOpacity: 0.3,
+    strokeColor: "#0000ff",
+    strokeOpacity: 0.6,
+    strokeWeight: 3,
+}
+
+export const polygonOptions = {
+    fillColor: "#ff0000",
+    fillOpacity: 0.3,
+    strokeColor: "#ff0000",
+    strokeOpacity: 0.6,
+    strokeWeight: 3,
+}
