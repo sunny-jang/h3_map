d45f1e6e95d08f536b6bff260f502be8779d7724
sunny-jang <esunbest@naver.com>
2023-01-26 00:56:43 +0900

    타입 정의

--- src/utils/mapUtil.ts
+++ src/utils/mapUtil.ts
@@ -1,17 +1,17 @@
 import * as h3 from 'h3-js';
 
-export const getH3Index = (e) => {
+export const getH3Index = (e:naver.maps.PointerEvent) => {
     const coord = e.coord;
     const hex = h3.latLngToCell(coord.y, coord.x, 10);
     return hex;
 };
 
-export const cellMaps = (data) => {
+export const cellMaps = (data:Hex[]) => {
     const indexList = data?.map((item) => item.index);
     return h3.cellsToMultiPolygon(indexList, true);
 };
 
-export const makePolyPath = (Multipolygon) => {
+export const makePolyPath = (Multipolygon:h3.CoordPair[][][]) => {
     const path = Multipolygon?.map(poly => {
         const innerPoly = poly[0].map((item) => {
             return new naver.maps.LatLng(item[1], item[0]);
